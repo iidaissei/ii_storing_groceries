1b348539fb9c21673ee21d15d5d817f9014d5038
issei_iida <issei.288.higasi@icloud.com>
2019-06-20 04:38:19 +0900

    2019/06/19

--- scripts/sg_navigation.py
+++ scripts/sg_navigation.py
@@ -2,6 +2,7 @@
 # -*- coding: utf-8 -*-
 
 import rospy
+import rosparam
 import tf 
 import math
 import actionlib
@@ -15,20 +16,12 @@ from sensor_msgs.msg import LaserScan
 
 class Navigation:
     def __init__(self):
-        # self.ac = actionlib.SimpleActionClient('move_base', MoveBaseAction)
-        # while not self.ac.wait_for_server(rospy.Duration(5)):
-        #     rospy.loginfo("Waiting for the move_base action server to comes up")
-        #     break
-        # rospy.loginfo("The server comes up")
-
         rospy.Subscriber('/navigation/memorize_place', String, self.getMemorizePlaceCB)
-        rospy.Subscriber('/navigation/command_place', String, self.getDestinationCB)
+        rospy.Subscriber('/navigation/move_place', String, self.getDestinationCB)
         self.sub_tf  = rospy.Subscriber('/tf', TFMessage, self.getTfCB)
         
         self.navigation_result_pub = rospy.Publisher('/navigation/result', Bool, queue_size = 1)
 
-        #rospy.get_param('shutdown_costmaps')
-        
         self.location_name = 'Null'
         self.location_list = []
         self.location_pose_x = 0
@@ -45,18 +38,27 @@ class Navigation:
     def getTfCB(self, receive_msg):
         self.sub_tf = receive_msg
 
+    def setCostmapParam(self,receive_msg):
+        costmap_param = String()
+        costmap_param = receive_msg
+        if(costmap_param == 'True'):
+            rospy.get_param('/move_base')
+            rospy.set_param('/move_base/shutdown_costmaps', costmap_param)
+            rospy.loginfo("Costmap OFF")
+        else:
+            rospy.get_param('/move_base')
+            rospy.set_param('/move_base/shutdown_costmaps', costmap_param)
+            rospy.loginfo("Costmap ON")
+
     def waitTopic(self):#------------------------------------------------------state 0
         while not rospy.is_shutdown():
-            #rospy.spin()
             if self.location_name != 'Null':
-                rospy.loginfo("Start LocationList setup")
+                rospy.loginfo("*Start LocationList setup*")
                 return 1
             elif self.destination != 'Null':
-                rospy.loginfo("Start navigation")
+                rospy.loginfo("*Start navigation*")
                 return 2
             else :
-                #rospy.spin()
-                #rospy.loginfo("Waiting for topic...")
                 return 0
 
     def setLocationList(self):#------------------------------------------------state 1
@@ -78,8 +80,10 @@ class Navigation:
     def navigateToDestination(self):#------------------------------------------state 2
         location_num = -1
         for i in range(len(self.location_list)):
+            print self.location_list[i][0]
+            print self.destination
             if self.destination == self.location_list[i][0]:
-                rospy.loginfo("Destination is " + self.location_name)
+                rospy.loginfo("Destination is " + self.destination)
                 location_num = i
         if location_num == -1:
             rospy.loginfo("Not found Destination")
@@ -113,24 +117,19 @@ class Navigation:
                 result.data = True
                 self.navigation_result_pub.publish(result)
                 rospy.loginfo("Published result")
-                #rospy.sleep(2.0)
+                self.setCostmapParam('False')
                 return 0
             elif ac.get_state() == 4:
                 rospy.loginfo("Buried in obstacle")
-                rospy.sleep(2.0)
+                rospy.sleep(1.0)
+                rospy.loginfo("Clear costmap")
                 return 3
 
     def clearCostmap(self):#---------------------------------------------------state 3
-        #rospy.get_param('move_base_params')
-        #rospy.set_param('shutdown_costmaps', True)
-        #rospy.loginfo("'shutdown_costmaps' changed True")
-        #while not rospy.is_shutdown():
-        #    rospy.loginfo("Clear costmap")
-        #    rospy.wait_for_service('move_base/clear_costmap')
-        #    clear_costmap = rospy.ServiceProxy('move_base/clear_costmap', std_srvs.srv.Empty)
-        #    rospy.sleep(5.0)
-        #    clear_costmap()
-           return 2
+        while not rospy.is_shutdown():
+            rospy.loginfo("*Clear Costmap*")
+            self.setCostmapParam('True')
+            return 2
 
 if __name__ == '__main__':
     rospy.init_node('sg_navigation', anonymous = True)
@@ -138,7 +137,6 @@ if __name__ == '__main__':
     state = 0
     while not rospy.is_shutdown():
         if state == 0:
-            #rospy.spin()
             state = nav.waitTopic()
         elif state == 1:
             state = nav.setLocationList()
